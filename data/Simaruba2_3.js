d8b5e681f089c0cee513ef48eaa235e175e19617
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-05-20 12:02:07 +0530

    Add files via upload

--- data/Simaruba2_3.js
+++ data/Simaruba2_3.js
@@ -3,7 +3,7 @@ var json_Simaruba2_3 = {
 "name": "Simaruba2_3",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Feet": 1.3, "Tree_Type": "Simaruba" }, "geometry": { "type": "Point", "coordinates": [ 84.02161327511115, 22.209510575290807 ] } },
-{ "type": "Feature", "properties": { "Feet": 1.369, "Tree_Type": "Simaruba" }, "geometry": { "type": "Point", "coordinates": [ 84.02160159012503, 22.209508124608817 ] } }
+{ "type": "Feature", "properties": { "Feet": 1.3, "Tree_Type": "Simaruba", "Tree_Growt": 0.21666666666699999 }, "geometry": { "type": "Point", "coordinates": [ 84.02161327511115, 22.209510575290807 ] } },
+{ "type": "Feature", "properties": { "Feet": 1.369, "Tree_Type": "Simaruba", "Tree_Growt": 0.228166666667 }, "geometry": { "type": "Point", "coordinates": [ 84.02160159012503, 22.209508124608814 ] } }
 ]
 }
